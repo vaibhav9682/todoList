2a70b2d096caaa0ae588d2a1222b37e8baf67021
vaibhav9682 <vaibhav205.vd@gmail.com>
2023-02-27 12:36:44 +0530

    css adding to task list

--- asset/JS/home.js
+++ asset/JS/home.js
@@ -1,1 +1,25 @@
-console.log("script is loaded")
+
+
+var btn = document.getElementsByClassName("showCategory");
+
+for (let i of btn) {
+    if (i.innerText == "cleaning") {
+        i.style.backgroundColor = "#566573";
+    }
+    else if(i.innerText == "personal"){
+        i.style.backgroundColor = "#73C6B6";
+
+    }
+    else if(i.innerText == "school"){
+        i.style.backgroundColor = "#F1C40F";
+
+    }
+    else if(i.innerText == "work"){
+        i.style.backgroundColor = "#85C1E9";
+
+    }
+    else if(i.innerText == "other"){
+        i.style.backgroundColor = "#AAB7B8";
+
+    }
+}
